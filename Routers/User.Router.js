647c3d133892a7399f82cf7d81efc81ad921c26e
RivianRithick <rithickshival16@gmail.com>
2024-04-07 13:30:07 +0530

    new Commit

---
+++ Routers/User.Router.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { AdminDashboard, ClickShortUrl,GetUrlcounts, ListAllUsers, Loginuser, RegisterUser, ResetPassword, forgotPassword, generateShortUrl } from '../Controllers/User.Controller.js'
+import authMiddleware from '../Middlewares/Auth.Middleware.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/register',RegisterUser)
+userRouter.post('/login',Loginuser)
+userRouter.get('/listallusers',ListAllUsers)
+userRouter.post('/forgotpassword',forgotPassword)
+userRouter.put('/resetpassword',ResetPassword)
+userRouter.post('/shorturl/:email',generateShortUrl)
+userRouter.get('/shortid/:shortUrl',ClickShortUrl)
+userRouter.get('/geturlcounts',GetUrlcounts)
+
+userRouter.get('/authorized',authMiddleware,AdminDashboard)
+
+export default userRouter
---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import ConnectDB from './Database/Config.js';
+import userRouter from './Routers/user.Router.js';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+ConnectDB();
+
+app.options('*', cors());
+
+app.use('/api/user', userRouter);
+
+app.listen(port, () => {
+    console.log(`App is listening on port-${port}`);
+});
